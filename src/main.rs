5d18f73b4ac5f214bb6b12724f2a96110eeddcb2
Alexander Nookinov <anookinov@gmail.com>
2021-09-04 03:04:34 +0700

    add fully qualified syntax for disambiguation by calling methods with the same time

--- src/main.rs
+++ src/main.rs
@@ -70,6 +70,52 @@ impl Add for Point {
     }
 }
 
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
+trait Animal {
+    fn baby_name() -> String;
+}
+
+struct Dog;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
+    }
+}
+
 fn main() {
     let mut num = 5;
 
@@ -138,4 +184,13 @@ fn main() {
 
         fn add(self, rhs: Rhs) -> Self::Output;
     }
+
+    let person = Human;
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+    person.fly(); // Human::fly(&person);
+
+    println!("A baby dog is called a {}", Dog::baby_name());
+    // println!("A baby dog is called a {}", Animal::baby_name()); // error as Rust can't figure out which implementation for Animal::baby_name we want
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name()); // fully qualified syntax i.e. <Type as Trait>::function(receiver_if_method, next_arg, ...);
 }
